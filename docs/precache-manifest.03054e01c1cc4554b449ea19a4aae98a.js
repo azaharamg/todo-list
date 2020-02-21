4d9a2a03d9e615136ec9b322b9cd5ac6f02fda24
Azahara <azaharamaga@gmail.com>
2020-02-21 19:37:48 +0100

    Create docs folder

---
+++ docs/precache-manifest.03054e01c1cc4554b449ea19a4aae98a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c24a4cd4fabf494d63473ff7a1d6a005",
+    "url": "./index.html"
+  },
+  {
+    "revision": "07004b51fdbd933a53c6",
+    "url": "./static/css/main.81844d4a.chunk.css"
+  },
+  {
+    "revision": "746fefade1761e1e85c6",
+    "url": "./static/js/2.d100f343.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.d100f343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07004b51fdbd933a53c6",
+    "url": "./static/js/main.e1ead466.chunk.js"
+  },
+  {
+    "revision": "3c0f1cf6754c3f776734",
+    "url": "./static/js/runtime-main.3f87527f.js"
+  }
+]);
