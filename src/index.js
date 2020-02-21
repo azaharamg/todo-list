b04bd7722c67e67b980ccb67f9901a6ee0d61f41
Azahara <azaharamaga@gmail.com>
2020-02-21 19:24:03 +0100

    Enable redux devtools

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import rootReducer from './reducers';
 import './stylesheet/index.css';
 import App from './components/App';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 render(
 	<Provider store={store}>
 		<App />
